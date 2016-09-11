060b120cbadad185c93ad3a8134653269d6b08b7
ZhengLiyi <zly0417@gmail.com>
2016-09-11 15:28:45 +0900

    2016.9.11 version

--- application/app/providers/settings-login/settings-login-local.ts
+++ application/app/providers/settings-login/settings-login-local.ts
@@ -3,39 +3,45 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
-  Generated class for the ProductListsGetSelectedProductLists provider.
+  Generated class for the SettingsLoginLocal provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
-export class getSelectedProductLists {
+export class SettingsLoginLocal {
   data: any;
 
   constructor(private http: Http) {
     this.data = null;
   }
 
-  load(k) {
+  load(username,password) {
     if (this.data) {
       // already loaded data
-      console.log(k.data.product);
+      console.log(username);
+      console.log(password);
       return Promise.resolve(this.data);
     }
 
+    var formData = new FormData();
+
+
     // don't have the data yet
     return new Promise(resolve => {
-    console.log(k.data.product);
       // We're using Angular Http provider to request the data,
       // then on the response it'll map the JSON data to a parsed JS object.
       // Next we process the data and resolve the promise with the new data.
-      this.http.get('https://randomuser.me/api/?results=10')
-        .map(res => res.json())
+
+      this.http.post("http://ec2-54-238-168-110.ap-northeast-1.compute.amazonaws.com:8080/api/login",
+      JSON.stringify({
+            "username": username,
+            "password": password
+      })
+      )
+        .map(res => res)
         .subscribe(data => {
-          // we've got back the raw data, now generate the core schedule data
-          // and save the data for later reference
-          this.data = data.results;
-          resolve(this.data);
+          console.log(data);
         });
     });
   }
